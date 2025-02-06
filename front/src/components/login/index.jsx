e6a175f00e47c45619114d0cd240f0fce4b592b4
Catifundo <lindomarbatistao@gmail.com>
2025-02-06 09:46:38 -0300

    06-02-25 9:45

--- front/src/components/login/index.jsx
+++ front/src/components/login/index.jsx
@@ -1,10 +1,32 @@
-import React from "react";
-
+import React, {useState} from "react";
+import './styles.css'
+import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
 
 export default function Login(){
+    const [user, setUser] = useState('')
+    const [password, setPassword] = useState('')
+
+
     return(
-        <>
-        <h1>Login</h1>
-        </>
+        <div className="container_login">
+            <h1>Login</h1>
+            <input
+                className="caixa"
+                placeholder="User"    
+                type="text"
+            />
+
+            <input
+                className="caixa"
+                placeholder="Password"
+                type="password"
+            
+            />
+
+            <button className="btn">
+                Enter
+            </button>
+        </div>
     )
 }

562aa3dd9c68c8f7f33656b5c3c7b9b2e2a85ff5
Catifundo <lindomarbatistao@gmail.com>
2025-02-11 09:19:49 -0300

    11-02-2025 9:19

---
+++ front/src/components/head/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../head/styles.css'
+
+export default function Head(){
+    return(
+        <main>
+            <section className="section">
+                <div className="title">
+                    <h2>Professores</h2>
+                </div>
+                <div className="nav">
+                    <span>Create</span>
+                    <span>Read</span>
+                    <span>Update</span>
+                    <span>Delete</span>
+                </div>
+            </section>
+        </main>
+    )
+}

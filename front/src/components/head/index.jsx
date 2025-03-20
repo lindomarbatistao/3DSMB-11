8a3fd64ef76fd7f49f66ea30f5a6090d70abcac5
Lindomar <lindomarbatistao@gmail.com>
2025-03-20 11:05:00 -0300

    20-03-2025 11:04 LOGOUT

--- front/src/components/head/index.jsx
+++ front/src/components/head/index.jsx
@@ -1,18 +1,32 @@
 import React from "react";
-import '../head/styles.css'
+import {ImExit} from 'react-icons/im'
+import './styles.css';
 
 export default function Head() {
+
+    const logout = ()=>{
+        localStorage.removeItem('access_token')
+        localStorage.removeItem('refresh_token')
+        window.location.href('/login')
+    }
+
+
     return (
-        <section className="container-head">
-            <div className="title">
-                <h2>Professores</h2>
-            </div>
-            <div className="nav">
-                <span>Create</span>
-                <span>Read</span>
-                <span>Update</span>
-                <span>Delete</span>
-            </div>
-        </section>
-    )
+        <div className="container_header">
+            <section className="body">
+                <div className="title">
+                    <h2>Professores</h2>
+                </div>
+                <div className="nav">
+                    <span>Create</span>
+                    <span>Read</span>
+                    <span>Update</span>
+                    <span>Delete</span>
+                </div>
+                <div className="exit">
+                    <ImExit onClick={logout}/>
+                </div>
+            </section>
+        </div>
+    );
 }
